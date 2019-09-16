82c737f0cb25c1e81c8b1389aba8ebb07fd411a1
NexRox <oscar-alejandro1@live.com>
2019-09-16 14:09:07 -0500

    C++

---
+++ Calculadora.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int n1, n2, suma, resta, multiplicacion, division;
+
+cout<<"Por favor digite el primer numero: "; cin>>n1;
+cout<<"Por favor digite el segundo numero: "; cin>>n2;
+
+suma=n1+n2;
+resta=n1-n2;
+multiplicacion=n1*n2;
+division=n1/n2;
+
+cout<<"El resultado de la suma es de :" <<suma <<endl;
+cout<<"El resultado de la resta es de :" <<resta <<endl;
+cout<<"El resultado de la multiplicacion es de :" <<multiplicacion <<endl;
+cout<<"El resultado de la division es de :" <<division <<endl;
+
+return 0;
+}
